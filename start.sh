b421904a51cf5dcfa1908931efe51d8bc73d28b9
w <somov.feodor.mail@gmail.com>
2023-11-02 01:37:45 +0300

    stable

--- start.sh
+++ start.sh
@@ -8,23 +8,29 @@ sudo cp -r ./xorg.conf.d/* /etc/X11/xorg.conf.d/
 sudo rm -rf ./xorg.conf.d/
 
 # move .config to ~
-sudo mv ./.config/* /home/$(whoami)/.config/
+sudo mv ./.config/* $HOME/config/
 rm -rf ./.config/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# move .xinitrc and .zshrc to ~
-mv .xinitrc .zshrc /home/$(whoami)/
+# move .xinitrc to ~
+mv .xinitrc $HOME
+
+# oh-my-zsh config
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv -f .zshrc $HOME
 
 # move wallpapper to ~ and set it 
-mv wp.png /home/$(whoami)/
-feh --bg-center /home/$(whoami)/wp.png
+mv wp.png $HOME
+feh --bg-center $HOME/wp.png
 
 rm -rf ../dotfiles/
 
 # install dwm
-cd /home/$(whoami)/.config/dwm/
+cd $HOME/.config/dwm/
 make
 sudo make install
 
-echo "done"
+echo "====="
+echo "Done!"
